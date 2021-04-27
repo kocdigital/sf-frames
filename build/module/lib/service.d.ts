c1694a6ae2fb767b8e9bf2b0f108da05c8ac8807
ksnet/09370844 <ozgur.cimen@ext.kocdigital.com>
2021-04-27 17:11:38 +0300

    packages fix

---
+++ build/module/lib/service.d.ts
@@ -0,0 +1,16 @@
+import { AxiosInstance } from 'axios';
+export interface Service {
+    createSecureInstance(baseUrl: string): AxiosInstance;
+    createUnsecureInstance(baseUrl: string): AxiosInstance;
+    getToken(): string;
+    getSettings(): any;
+    setConfig(value: any): any;
+    signOut(): any;
+    checkUser(): any;
+    store: any;
+    vuetify: any;
+    dialog: any;
+    router: any;
+    setTheme(isDark: boolean): void;
+    onThemeChange(isDark: boolean): void;
+}
